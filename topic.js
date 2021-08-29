3fef49ab70a044920c42cfbff51efaf2fff91f7b
Yasien Dwieb <yasien.dwieb@gmail.com>
2021-08-29 15:51:29 +0200

    feat: add create topic script

---
+++ topic.js
@@ -0,0 +1,30 @@
+const { Kafka } = require('kafkajs')
+
+run()
+
+async function run() {
+    try {
+        const kafka = new Kafka({
+            brokers: ["localhost:9092"]
+        })
+        const admin = kafka.admin()
+        
+        console.log('Conntecting...')
+        await admin.connect()
+        console.log('Conntected...')
+
+        admin.createTopics({
+            topics: [{
+                topic: "Users",
+                numPartitions: 2
+            }]
+        })
+
+        await admin.disconnect()
+        console.log('Disconntected...')
+    } catch (error) {
+        console.log(error)
+    } finally {
+        process.exit(0)
+    }
+}
